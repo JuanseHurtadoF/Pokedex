50df1efed5d8dd7dc672bf0b1efd84d1349c61b0
JuanseHurtadoF <juansebastianhurtadof@gmail.com>
2023-01-18 10:25:46 -0500

    dynamic imports set

---
+++ src/components/sections/hero/index.tsx
@@ -0,0 +1,11 @@
+import styles from '@styles/sections/Hero.module.scss'
+
+const Hero = () => {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
+
+export default Hero;
