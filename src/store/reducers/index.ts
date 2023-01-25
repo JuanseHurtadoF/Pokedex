ebd6ba73a7ce4e12b3c79bc143455f84dee95562
Juanse Hurtado <31664711+JuanseHurtadoF@users.noreply.github.com>
2023-01-25 12:58:44 -0500

    Redux set up (#5)
    * installed dependencies
    
    * redux working
    
    * redux setup with TS
    
    * redux final adjustments

---
+++ src/store/reducers/index.ts
@@ -0,0 +1,38 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {
+  name: "Juanse",
+  id: 1,
+  image: "image",
+  types: ["fire", "rock"],
+  stats: [
+    {
+      name: "hp",
+      value: 50,
+    },
+    {
+      name: "defense",
+      value: 55,
+    },
+    {
+      name: "attack",
+      value: 70,
+    },
+  ],
+};
+
+export default function reducer(
+  state = initialState,
+  action: PayloadAction<string>
+) {
+  switch (action.type) {
+    case "CHANGE_NAME":
+      return {
+        ...state,
+        name: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
